316bae6d5a3ccde57e792a3c0faa2813c55e28c1
sonalijain263 <sonalijain263@gmail.com>
2021-10-13 15:15:41 +0530

    9th change

--- DemoTestOnJenkins/src/test/java/demo/DemoTest.java
+++ DemoTestOnJenkins/src/test/java/demo/DemoTest.java
@@ -1,8 +1,12 @@
 package demo;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -11,14 +15,11 @@ public class DemoTest {
 	public WebDriver driver;
 	
 	   @Test
-		public void setup()
+		public void setup() throws MalformedURLException
 		{
 	  	
 		System.setProperty("webdriver.chrome.driver","/usr/bin/google-chrome");
-		 ChromeOptions options = new ChromeOptions();
-	        options.addArguments("headless");
-	        options.addArguments("disable-gpu");	
-		driver = new ChromeDriver(options);
+		 driver = new RemoteWebDriver(new URL("http://127.0.0.1:9515"),new ChromeOptions()); 
 	         driver.get("https://www.pionglobal.com/");
 	       
 	         driver.manage().window().maximize();
