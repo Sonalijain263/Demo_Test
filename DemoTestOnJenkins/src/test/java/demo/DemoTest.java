1742e2b797d674d2966b7ecf4c87d3b211671819
Sonalijain263 <83583118+Sonalijain263@users.noreply.github.com>
2021-10-13 14:14:37 +0530

    6th change

--- DemoTestOnJenkins/src/test/java/demo/DemoTest.java
+++ DemoTestOnJenkins/src/test/java/demo/DemoTest.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 public class DemoTest {
 	
