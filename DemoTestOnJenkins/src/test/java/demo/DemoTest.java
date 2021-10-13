1b94b94bc86b715387eb9a97b7b144be352dbd95
Sonalijain263 <sonalijain263@gmail.com>
2021-10-13 13:24:30 +0530

    1st commit

---
+++ DemoTestOnJenkins/src/test/java/demo/DemoTest.java
@@ -0,0 +1,31 @@
+package demo;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class DemoTest {
+	
+	public WebDriver driver;
+	
+	   @Test
+		public void setup()
+		{
+	  	
+		System.setProperty("webdriver.chrome.driver","E:\\chromedriver_win32\\chromedriver.exe");
+		 ChromeOptions options = new ChromeOptions();
+	       options.addArguments("headless");
+	       options.addArguments("disable-gpu");	
+		driver = new ChromeDriver(options);
+	 
+	         driver.get("https://www.pionglobal.com/");
+	       
+	         driver.manage().window().maximize();
+	         
+	         System.out.println("Title of the page is: " + driver.getTitle());
+	         Assert.assertTrue(driver.getTitle().equals("Pion Global"));
+	            
+		}
+}
