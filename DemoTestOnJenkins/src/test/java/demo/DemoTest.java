4daf5c1f681ddde75991afc0ca16676a69e2068c
sonalijain263 <sonalijain263@gmail.com>
2021-10-14 14:43:38 +0530

    20th change

--- DemoTestOnJenkins/src/test/java/demo/DemoTest.java
+++ DemoTestOnJenkins/src/test/java/demo/DemoTest.java
@@ -7,11 +7,10 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-
-import io.github.bonigarcia.wdm.WebDriverManager;
 
 
 
@@ -22,13 +21,18 @@ public class DemoTest {
 	   @Test
 		public void setup() throws MalformedURLException
 		{
-		   WebDriverManager.chromedriver().setup();
-	//	System.setProperty("webdriver.chrome.driver","/usr/bin/google-chrome");
-		 driver = new RemoteWebDriver(new URL("http://139.59.77.9:4444/"),new ChromeOptions()); 
-	    ChromeOptions options= new ChromeOptions();
+//		System.setProperty("webdriver.chrome.driver","/usr/bin/google-chrome");
+//		 System.setProperty("webdriver.chrome.driver","E:\\chromedriver_win32\\chromedriver.exe");
+//		driver = new RemoteWebDriver(new URL("http://139.59.77.9:4444/"),new ChromeOptions()); 
+//	    ChromeOptions options= new ChromeOptions();
+// 	    options.addArguments("headless");
+//		driver= new ChromeDriver(options);  
+		DesiredCapabilities capability = DesiredCapabilities.chrome();
+		driver = new RemoteWebDriver(new URL("http://139.59.77.9:4444/"), capability);
+		ChromeOptions options= new ChromeOptions();
  	    options.addArguments("headless");
-		driver= new ChromeDriver(options);    
-	driver.get("https://www.pionglobal.com/");
+		driver= new ChromeDriver(options);
+		driver.get("https://www.pionglobal.com/");
 	       
 	         driver.manage().window().maximize();
 	         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);   
