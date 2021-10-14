9b8db6bc3b08d69b4abcc21fa7b86fb948a29f46
Sonalijain263 <83583118+Sonalijain263@users.noreply.github.com>
2021-10-14 12:10:05 +0530

    Update DemoTest.java

--- DemoTestOnJenkins/src/test/java/demo/DemoTest.java
+++ DemoTestOnJenkins/src/test/java/demo/DemoTest.java
@@ -23,7 +23,15 @@ public class DemoTest {
 //		 driver = new RemoteWebDriver(new URL("http://139.59.77.9:8080/"),new ChromeOptions()); 
  //	    ChromeOptions options= new ChromeOptions();
 // 	    options.addArguments("--no-sandbox");
-		driver= new ChromeDriver();    
+			ChromeOptions options = new ChromeOptions();
+	options.addArguments("start-maximized"); // open Browser in maximized mode
+	options.addArguments("disable-infobars"); // disabling infobars
+	options.addArguments("--disable-extensions"); // disabling extensions
+	options.addArguments("--disable-gpu"); // applicable to windows os only
+	options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
+	options.addArguments("--no-sandbox"); // Bypass OS security model
+	driver = new ChromeDriver(options);
+	//	driver= new ChromeDriver();    
 		driver.get("https://www.pionglobal.com/");
 	       
 	         driver.manage().window().maximize();
